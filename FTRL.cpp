c229dd3f34d6988700782cfff047da9eec2643ab
zalivnaja <zalivnaja@gmail.com>
2017-05-01 12:59:57 +0300

    implement categorical features

--- FTRL.cpp
+++ FTRL.cpp
@@ -7,29 +7,70 @@
 #include <sstream>
 #include <algorithm>
 #include <iterator>
+#include <functional>
+#include <exception>
 
 using namespace std;
 
 
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
+
+
+std::string get_hash_feature(const std::string& _value, int _hash_size)
+{
+    static std::hash<std::string> hash_fn;
+
+    int hash_value = hash_fn(_value);
+    size_t hash = abs(hash_value) % _hash_size;
+    return to_string(hash);
+}
+
+
 struct Row
 {
-
 public:
-    void fillFromStr(const std::string& _columns, const std::string& _row)
+
+    void fillFromStr(const std::vector<std::string>& _columns, const std::string& _row, int _categ_features_count, int _hash_size)
     {
         std::istringstream ss(_row);
         std::string token;
 
-        int comp_count = std::count(_row.begin(), _row.end(), ',');
-        components_.resize(comp_count);
+        int comp_count = _columns.size() - 1;
 
         int current_comp = 0;
         while(std::getline(ss, token, ',')) 
         {
             if (current_comp + 1 < comp_count)
-                components_[current_comp++] = ::atof(token.c_str());
-            else
+            {
+                auto value = token.c_str();
+                if (current_comp < _columns.size() - _categ_features_count)
+                {
+                    components_[_columns[current_comp]] = ::atof(value);
+                }
+                else
+                {
+                    components_[_columns[current_comp] + "_" + get_hash_feature(value, _hash_size)] = 1;
+                }
+
+                ++current_comp;
+            } else {
                 target_ = ::atof(token.c_str());
+            }
         }
     }
 
@@ -37,7 +78,7 @@ public:
     {
     }
 
-    std::vector<float> components_;
+    std::unordered_map<string, float> components_;
     float target_;
 };
 
@@ -47,24 +88,28 @@ private:
     std::string filename_;
     ifstream data_file_;
     std::string line_;
-    std::string columns_;
 
     std::string last_error_;
+    int categ_features_count_;
 
 public:
-    DataProvider(const std::string& _filename)
+    DataProvider(const std::string& _filename, int _categ_features_count, int _bits)
         : filename_(_filename)
         , data_file_(filename_)
+        , categ_features_count_(_categ_features_count)
+        , bits_(_bits)
     {
         if (data_file_.is_open())
         {
-            if (!getline(data_file_, columns_))
+            std::string columns_row;
+            if (!getline(data_file_, columns_row))
             {
                 last_error_ = "cann't read head";
                 data_file_.close();
             }
 
-            columns_count_ = std::count(columns_.begin(), columns_.end(), ',') - 1; // id and target
+            columns_ = split(columns_row, ',');
+            numerical_columns_count_ = columns_.size() - 2 - _categ_features_count; // id and target
             // std::cout << "columns_count_ " << columns_count_ << std::endl;
         }
         else
@@ -79,7 +124,7 @@ public:
         {
             if (getline(data_file_, line_))
             {
-                _row->fillFromStr(columns_, line_);
+                _row->fillFromStr(columns_, line_, categ_features_count_, get_hashed_features_count());
                 return true;
             }
             else
@@ -94,7 +139,20 @@ public:
         }
     }
 
-    int columns_count_;
+    int get_hashed_features_count() const
+    {
+        auto result = (2 << bits_) - numerical_columns_count_;
+        if (result < 0)
+        {
+            throw std::runtime_error("Too small amount of bits.");
+        }
+
+        return result; 
+    }
+
+    int bits_;
+    std::vector<std::string> columns_;
+    int numerical_columns_count_;
 };
 
 
@@ -125,24 +183,28 @@ float logloss(float _p, float _y, float _eps = 1e-5)
 class FTRL
 {
 public:
-    FTRL(float _alpha, float _beta, float _L1, float _L2, int _columns_count)
-        : alpha_(_alpha)
+    FTRL(int _bits, float _alpha, float _beta, float _L1, float _L2, int _numerical_columns_count
+        , std::vector<std::string> _columns, int _categ_features_count)
+        
+        : bits_(_bits)
+        , alpha_(_alpha)
         , beta_(_beta)
         , L1_(_L1)
         , L2_(_L2)
-        , columns_count_(_columns_count)
-        , n_(columns_count_, 0.0)
-        , z_(columns_count_, 0.0)
-    {}
+        , numerical_columns_count_(_numerical_columns_count)
+        , columns_(_columns)
+        , categ_features_count_(_categ_features_count)
+    {
+    }
 
     float predict(const Row& _x)
     {
         w_.clear();
 
         float wx = 0;
-        int i = 0;
-        for (auto x_i : _x.components_)
+        for (auto& x_i : _x.components_)
         {
+            auto& i = x_i.first;
             if (abs(z_[i]) <= L1_)
             {
                 w_[i] = 0;
@@ -152,8 +214,7 @@ public:
                 float sign = z_[i] < 0 ? -1 : 1;
                 w_[i] = (sign * L1_ - z_[i]) / ((beta_ + sqrt(n_[i])) / alpha_ + L2_);
             }
-            wx += w_[i] * x_i;
-            ++i;
+            wx += w_[i] * x_i.second;
         }
 
         return sigmoid(wx);
@@ -163,33 +224,45 @@ public:
     {
         float g = p - y;
 
-        int i = 0;
-        for (auto comp : x.components_)
+        for (auto& comp : x.components_)
         {
-            auto g_i = g * comp;
+            auto& i = comp.first;
+
+            auto g_i = g * comp.second;
             auto sigma_i = (sqrt(n_[i] + g_i * g_i) - sqrt(n_[i])) / alpha_;
             z_[i] += g_i - sigma_i * w_[i];
             n_[i] += g_i * g_i;
-            ++i;
         }
     }
 
 private:
+    int bits_;
     float alpha_;
     float beta_;
     float L1_;
     float L2_;
-    int columns_count_;
+    int numerical_columns_count_;
+    int categ_features_count_;
+    std::vector<std::string> columns_;
 
-    vector<float> n_;
-    vector<float> z_;
-    unordered_map<int, float> w_;
+    unordered_map<string, float> n_;
+    unordered_map<string, float> z_;
+    unordered_map<string, float> w_;
 };
 
 int main()
 {
-    DataProvider dataProvider("testdata.csv");
-    FTRL ftrl(.005, 1, 0, 1, dataProvider.columns_count_);
+    int bits = 4;
+
+    // TODO : unhard-code it
+    // int categ_features_count = 0;
+    // const string& filename = "testdata.csv";
+
+    const string& filename = "categ_testdata.csv";
+    int categ_features_count = 10;
+
+    DataProvider dataProvider(filename, categ_features_count, bits);
+    FTRL ftrl(bits, .005, 1, 0, 1, dataProvider.numerical_columns_count_, dataProvider.columns_, categ_features_count);
 
     Row row;
     int i = 0;
@@ -199,7 +272,7 @@ int main()
     {
         float loss = 0;
 
-        DataProvider dataProvider("testdata.csv");
+        DataProvider dataProvider(filename, categ_features_count, bits);
         while (dataProvider.TryGetNextRow(&row))
         {
             float p = ftrl.predict(row);
--- datagen.py
+++ datagen.py
@@ -14,11 +14,16 @@ def sigmoid(x, eps = 35.0):
         return z / (1 + z)
 
 def generate_simple_test(filename):
-    w_size = 10
-    samples_count = 100
+    w_num_size = 10
+    samples_count = 1000
     bernoulli_count = 10
     
-    columns = ["id"] + ['var_' + str(i) for i in range(w_size)] + ["target"]
+    categorical_size = 5
+    categorical_count = 10
+    
+    w_size = w_num_size + categorical_size * categorical_count
+    
+    columns = ["id"] + ['var_' + str(i) for i in range(w_num_size + categorical_count)] + ["target"]
     with open(filename, 'wt') as testfile:
         testfile.write(','.join(columns) + '\n') 
     
@@ -26,14 +31,29 @@ def generate_simple_test(filename):
         b = np.random.rand(1, 1)[0][0] - 3
 
         ind = 0
-        for i in range(samples_count):
-            x = np.random.rand(1, w_size)
-            p = sigmoid(x.dot(w) + b)
+        for sample_i in range(samples_count):
+            x = np.random.rand(1, w_num_size)
+            
+            cat_x = np.random.choice(categorical_size, categorical_count)
+            cat_x = np.reshape(cat_x, (1, categorical_count))
+            
+            dummy_cat_x = np.zeros((1, categorical_size * categorical_count))
+            for cat_i in range(categorical_count):
+                dummy_cat_x[0, cat_i * categorical_size + cat_x[0, cat_i]] = 1
+            
+            full_dummy_x = np.concatenate([x, dummy_cat_x], axis = 1)
+            full_x = np.concatenate([x, cat_x], axis = 1)
+            
+            p = sigmoid(full_dummy_x.dot(w) + b)
             ys = bernoulli.rvs(p, size=bernoulli_count)
             
-            str_x = np.array2string(x[0], precision=2, separator=',', suppress_small=True)
+            str_x = ', '.join(["{0:.2f}".format(x_i) for x_i in x[0]]) + ', ' + ', '.join([str(x_i) for x_i in cat_x[0]])
+            # str_x = np.array2string(full_x[0], precision=2, separator=',', suppress_small=True)
+
+            # print(str_x)
+
             for y in ys:
                 testfile.write(str(ind) + ',' + str_x[1:-1] + ',' + str(y) + '\n') 
                 ind += 1
         
-generate_simple_test('testdata.csv')
+generate_simple_test('categ_testdata.csv')
